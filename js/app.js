18110b99f7ca2abfc304fd03c1e9e982ec3032d7
Saul <sacasher@gmail.com>
2016-09-10 22:13:13 -0500

    Solution

--- js/app.js
+++ js/app.js
@@ -1,12 +1,36 @@
 (function() {
   'use strict';
-  var x = "hello";
+  angular.module('LunchCheck',[])
+    .controller("LunchController",LunchController);
 
-  angular.module('myFirstApp',[])
-  .controller("MyFirstController",function ($scope) {
-   $scope.name = "Markov";
-   $scope.sayHello = function () {
-     return "Hello Coursera";
-   };
-  });
+  LunchController.$inject = ['$scope'];
+
+  function LunchController($scope) {
+    $scope.menu= ""; 
+    $scope.result= "";
+    $scope.check= function() {
+      var content = $scope.menu;
+      var result = "";
+      if (content.replace(/,/g," ").trim().length < 1) {
+        document.getElementById("lunch-menu")
+          .style.border = "2px solid red";
+        document.getElementsByClassName("message")[0]
+          .style.color = "red";
+        result = "Please enter data first";
+      }else{
+        var array;
+        array = content.split(',');
+        if (array.length < 4) {
+          result = "Enjoy!";
+        }else{
+          result = "Too much!";
+        }
+        document.getElementById("lunch-menu")
+          .style.border = "2px solid green";
+        document.getElementsByClassName("message")[0]
+          .style.color = "green";
+      }
+      $scope.result=result;
+    };
+  }
 })();
